420cbd817593ee2bc01ec675aabccdd9bd7d0b27
Lucas Malta <lucas50xd@hotmail.com>
2017-09-28 21:09:46 -0300

    refactoring menu

--- main.pl
+++ main.pl
@@ -1,33 +1,64 @@
 :- dynamic player/2.
 :- [aux].
+
+ownedPokemon([]) :-
+  false.  % Usuário começa com uma lista vazia de pokemons
+
 play() :-
         write('Digite o seu nome:'),
         nl,
         read(Name),
+        assert(player(Name)),
         nl,
+        menu().
+
+menu() :-
+  write('Digite a opção dejada:'),
+  nl,
+  write('1- Cadastrar um pokemon'),
+  nl,
+  write('2- Listar meus pokemons'),
+  nl,
+  write('3- Buscar o melhor campo para treino'),
+  nl,
+  read(Choice),
+  option(Choice).
+
+
+option(1) :-
+    choosePokemon().
+
+option(2) :-
+    findall(X, ownedPokemon(X), L),
+    write(L),
+    nl,
+    menu().
+
+choosePokemon() :-
         write('Digite o pokemon escolhido! Opções:'),
         nl,
         write('pikachu'), nl,
         write('charizard'), nl,
         write('bulbassauro'), nl,
         read(Pokemon),
-        checkpokemon(Name, Pokemon).
+        checkpokemon(Pokemon).
 
-retry(Name) :-
+retry() :-
         write('Pokemon inválido!! Selecione um pokemon da lista:'),
         nl,
         write('pikachu'), nl,
         write('charizard'), nl,
         write('bulbassauro'), nl,
         read(Pokemon),
-        checkpokemon(Name, Pokemon).
+        checkpokemon(Pokemon).
 
 
-checkpokemon(Name, Pokemon) :- ifThenElse(ispokemon(Pokemon), setpokemon(Name, Pokemon), retry(Name)).
+checkpokemon(Pokemon) :- ifThenElse(ispokemon(Pokemon), setpokemon(Pokemon), retry()).
 
 
-setpokemon(Name, Pokemon) :-
-        assert(player(Name, pokemon(Pokemon))).
+setpokemon(Pokemon) :-
+        assert(ownedPokemon(pokemon(Pokemon))),
+        menu().
 
 
 pokemon(pikachu).
@@ -36,4 +67,4 @@ pokemon(bulbassauro).
 
 
 
-tipo_de_ataque(pikachu, raio).
+tipo_de_ataque(pikachu, raio). %exemplo
